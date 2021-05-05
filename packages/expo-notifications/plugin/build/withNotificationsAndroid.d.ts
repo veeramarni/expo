b216ae479a156ed706ef1a5fbc37746ab1dc8eaa
cruzach <charliecruzan@gmail.com>
2021-05-05 16:51:49 -0400

    remove fs-extra, move some methods from async to sync

--- packages/expo-notifications/plugin/build/withNotificationsAndroid.d.ts
+++ packages/expo-notifications/plugin/build/withNotificationsAndroid.d.ts
@@ -1,4 +1,4 @@
-import { AndroidConfig, ConfigPlugin } from '@expo/config-plugins';
+import { ConfigPlugin } from '@expo/config-plugins';
 import { ExpoConfig } from '@expo/config-types';
 import { NotificationsPluginProps } from './withNotifications';
 export declare const META_DATA_NOTIFICATION_ICON = "expo.modules.notifications.default_notification_icon";
@@ -26,13 +26,9 @@ export declare function getNotificationColor(config: ExpoConfig): string | null;
  * Applies notification icon configuration for expo-notifications
  */
 export declare function setNotificationIconAsync(icon: string | null, projectRoot: string): Promise<void>;
-export declare function setNotificationConfigAsync(props: {
-    icon: string | null;
-    color: string | null;
-}, manifest: AndroidConfig.Manifest.AndroidManifest): Promise<AndroidConfig.Manifest.AndroidManifest>;
 export declare function setNotificationIconColorAsync(color: string | null, projectRoot: string): Promise<void>;
 /**
- * Save sound files to <project-root>/android/app/src/main/res/raw
+ * Save sound files to `<project-root>/android/app/src/main/res/raw`
  */
-export declare function setNotificationSoundsAsync(sounds: string[], projectRoot: string): Promise<void>;
+export declare function setNotificationSounds(sounds: string[], projectRoot: string): void;
 export declare const withNotificationsAndroid: ConfigPlugin<NotificationsPluginProps>;
